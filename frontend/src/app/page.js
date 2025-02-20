a3ef32b9144ec23e5dc9bf0d5eed11fbd4257a5b
unknown <bijay@groonyindia.in>
2025-02-20 08:40:43 -0800

    bug fixed in pages.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import context from "react-bootstrap/esm/AccordionContext";
 
 export default function UserList() {
   const [users, setUsers] = useState([]);
